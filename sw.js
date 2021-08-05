ad1a9df67d77e136b48d61ffb77d45d6a7cd8c97
david <simplesam8856@gmail.com>
2021-08-05 16:39:46 +1000

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-8f9215f534ac5a7c4960.js"
+    "url": "webpack-runtime-d9db671159d764a1218b.js"
   },
   {
     "url": "framework-c8d3a5345e3f321d7da5.js"
@@ -46,7 +46,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "7935d6dd00a76499efc74c9171122751"
+    "revision": "3a1520a704abadf2cc021f6527a75dd7"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-77466cc0b3ed4dac08eb.js"
